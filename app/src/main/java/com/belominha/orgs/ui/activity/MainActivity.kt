274a058b563ac3cc548c9da40ac4808eada0fed0
Aline <abelomo95@gmail.com>
2023-02-13 13:32:48 -0300

    AndroidStudio resources and build settings

--- app/src/main/java/com/belominha/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/belominha/orgs/ui/activity/MainActivity.kt
@@ -1,35 +1,22 @@
 package com.belominha.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.belominha.orgs.R
 import com.belominha.orgs.model.Product
 import com.belominha.orgs.ui.recyclerview.adapter.ProductListAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ProductListAdapter(this, listOf(
             Product("test","test desc", BigDecimal("19.99")),
             Product("test1","test desc1", BigDecimal("29.99"))
         ))
-
-
-
-        //recyclerView.layoutManager = LinearLayoutManager(this)
-
-        /*val title = findViewById<TextView>(R.id.title)
-        title.text = "Cesta de Frutas"
-        val description = findViewById<TextView>(R.id.description)
-        description.text = "Laranja, manga e maçã"
-        val value = findViewById<TextView>(R.id.value)
-        value.text = "19.99"*/
     }
 
 }
---
+++ app/src/main/java/com/belominha/orgs/ui/activity/ProductFormActivity.kt
@@ -0,0 +1,8 @@
+package com.belominha.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.belominha.orgs.R
+
+class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
+}
