2cb9ac1cf9eec3fa183894240669c6748e674148
Aline <abelomo95@gmail.com>
2023-02-13 08:50:13 -0300

    first commit

---
+++ app/src/main/java/com/belominha/orgs/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.belominha.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("cesta de frutas")
+        setContentView(view)
+
+    }
+
+}
