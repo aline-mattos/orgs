dd39ad83a9d8d8f74e819412b491674f6ee2a371
Aline <abelomo95@gmail.com>
2023-02-14 19:22:48 -0300

    adding floatactionbutton

--- app/src/main/java/com/belominha/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/belominha/orgs/ui/activity/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.belominha.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.belominha.orgs.R
 import com.belominha.orgs.model.Product
 import com.belominha.orgs.ui.recyclerview.adapter.ProductListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -17,6 +19,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             Product("test","test desc", BigDecimal("19.99")),
             Product("test1","test desc1", BigDecimal("29.99"))
         ))
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, ProductFormActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
